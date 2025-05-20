8884c97576b12719f33c5b4921a9dc97b7b9b5b5
Tharusha Jayamanna <tharushajayamanna60@gmail.com>
2025-05-20 15:28:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ from page import router as page_router
 app = FastAPI()
 
 app.include_router(page_router)
+
+from models import Base
+from database import engine
+
+Base.metadata.create_all(bind=engine)
